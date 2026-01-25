97fe551fd8dadbbbde43a760b9f8df3d40f7664d
yue.shanfei <yue.shanfei@iwhalecloud.com>
2026-01-25 17:05:44 +0800

    一键打包

--- mvn.sh
+++ mvn.sh
@@ -14,15 +14,3 @@ ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
 echo "编译服务"
 mvn clean package -DskipTests -Dskip.frontend.build=true
 echo "启动服务"
-nohup java -jar travel/target/travel-platform-1.0.0.jar \
---server.port=8081 \
---spring.profiles.active=prod \
---MYSQL_PWD= \
---WECHAT_APPID=wxec792e1911ef2bde \
---WECHAT_SECRET=0275a3dc14da6e1280837611d98fea66 \
---OSS_ENABLED=true \
---OSS_ENDPOINT=oss-cn-beijing.aliyuncs.com \
---OSS_ACCESS_KEY_ID=LTAI5t9mJwcKfpxwfptvPkNT \
---OSS_ACCESS_KEY_SECRET=Byp7tivKt0jv9j3l9Huh8Avertm0S5 \
---OSS_BUCKET_NAME=wuqiao-travel
-  2>&1 &
